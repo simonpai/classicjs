cc6b47dc25a5d80ff187a19f61b4c2ca519906b5
simonpai <simonpai@potix.com>
2014-02-18 00:31:32 +0800

    Add test case.

--- test/class.js
+++ test/class.js
@@ -37,7 +37,7 @@ describe('Extend', function() {
 	});
 	
 	describe('overridden members', function() {
-		it('Extended class should override members as expected', function() {
+		it('Extended class should override members as expected.', function() {
 			var a = new A(),
 				b = new B(20),
 				c = new C(40, 80);
@@ -54,7 +54,7 @@ describe('Extend', function() {
 	});
 	
 	describe('suber object', function() {
-		it('Extended class should have correct super object references', function() {
+		it('Extended class should have correct super object references.', function() {
 			
 			var A = cj.extend(Object, {
 				r: function () {
@@ -78,6 +78,37 @@ describe('Extend', function() {
 			assert.equal(10, a.r());
 			assert.equal(20, b.r());
 			assert.equal(30, c.r());
+		});
+	});
+	
+	describe('explicit super constructor', function() {
+		it('Explicit super constructor pattern should work as expected.', function() {
+			var Z = cj.extend(Object, {
+				y: function () {
+					return 0;
+				}
+			});
+			var A = cj.extend(Z, function (x) {
+				this.x = x;
+				return {
+					y: function () {
+						return 10;
+					}
+				};
+			});
+			var B = cj.extend(function () {
+				return new A(10);
+			}, {
+				y: function () {
+					return 20;
+				}
+			});
+			
+			var b = new B();
+			assert.equal(10, b.x);
+			assert.equal(10, b.super.y());
+			assert.equal(0, b.super.super.y());
+			
 		});
 	});
 	
